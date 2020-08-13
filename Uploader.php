2e3a43060b0a98d37ae3dc857f45116e85880cae
sonniger <69643480+sonniger@users.noreply.github.com>
2020-08-13 18:37:00 +0200

    Create Uploader.php

---
+++ Uploader.php
@@ -0,0 +1,14 @@
+<?php
+class Uploader {
+    public $filename = "";
+   
+   
+    function upload() {
+        // uploads a file anonymously to bianco.tv
+        // TODO
+        echo "Uploading" . $filename;
+    }
+}
+
+$foo = new Foo;
+?>
